0a4a9bd4a6082aa2e5e9d68c0b36f0f8ef2a4822
Syalabi <syalabi.seet@hotmail.com>
2022-05-31 16:45:30 +0800

    model architecture done

--- src/dataset.py
+++ src/dataset.py
@@ -1,15 +1,106 @@
+import os
+import glob
+from tqdm import tqdm
 import cv2
+import albumentations as A
 
 import tensorflow as tf
 
-class Augmentor:
-    def __init__(self):
-        return
-
 class TFRWriter:
     def __init__(self):
-        return
+        self.main_dir = "sRGB2XYZ"
+        self.save_path = os.path.join(self.main_dir, "shards")
+        self.resize_shape = (256, 256)
+        self.transform = A.Compose(
+            [
+                A.Flip(),
+                A.RandomCrop(256, 256)
+            ],
+            additional_targets={'XYZ_image': 'image'}
+        )
+
+    def _bytes_feature(self, value):
+        """Returns a bytes_list from a string / byte."""
+        if isinstance(value, type(tf.constant(0))):
+            value = value.numpy()
+        return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+    
+    def serialize_example(self, sample):
+        feature = {
+            "XYZ_image": self._bytes_feature(sample[0]),
+            "sRGB_image": self._bytes_feature(sample[1]),
+            "filename": self._bytes_feature(sample[2])}
+
+        example_proto = tf.train.Example(
+            features=tf.train.Features(feature=feature))
+        return example_proto.SerializeToString()
+
+
+    def get_image(self, sample_path):
+        image = cv2.imread(sample_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, self.resize_shape)
+        return image
+
+    def get_samples(self, train_set):
+        path = f"{self.main_dir}/sRGB_{train_set}/*.JPG"
+        return glob.glob(path)
+
+    def get_sample_data(self, sample):
+        xyz_filename = sample.replace("sRGB_", "XYZ_").replace(".JPG", ".png")
+        assert os.path.exists(xyz_filename), f"{xyz_filename} file does not exist."
+
+        srgb_image = self.get_image(sample)
+        xyz_image = self.get_image(xyz_filename)
+        transformed = self.transform(image=srgb_image, XYZ_image=xyz_image)
+        filename = os.path.basename(sample)
+                
+        return [
+            tf.io.serialize_tensor(transformed['XYZ_image'], name="XYZ_image"),
+            tf.io.serialize_tensor(transformed['image'], name="sRGB_image"),
+            tf.io.serialize_tensor(filename, name="filename")]
+
+    def write(self):
+        for train_set in ["training", "validation", "testing"]:
+            shard_path = os.path.join(self.save_path, f"{train_set}.tfrec")
+            samples = self.get_samples(train_set)
+            with tf.io.TFRecordWriter(shard_path) as f:
+                for sample in tqdm(samples, total=len(samples), desc=f"{train_set}"):
+                    sample_data = self.get_sample_data(sample)
+                    f.write(self.serialize_example(sample_data))
 
 class DataLoader:
-    def __init__(self):
-        return
+    def __init__(self, batch_size=8):
+        self.batch_size = batch_size
+        self.buffer_size = 64
+
+    def read_tfrecord(self, example):
+        feature_description = {
+            'XYZ_image': tf.io.FixedLenFeature([], tf.string),
+            'sRGB_image': tf.io.FixedLenFeature([], tf.string),
+            'filename': tf.io.FixedLenFeature([], tf.string)}
+        
+        example = tf.io.parse_single_example(example, feature_description)
+
+        for key in example:
+            if key != 'filename':
+                example[key] = tf.io.parse_tensor(example[key], out_type=tf.uint8)
+                example[key] = tf.cast(example[key], dtype=tf.float32)
+            else:
+                example[key] = tf.io.parse_tensor(example[key], out_type=tf.string)
+        return example
+    
+    def load_dataset(self, files):
+        ignore_order = tf.data.Options()
+        ignore_order.experimental_deterministic = False
+        dataset = tf.data.TFRecordDataset(files)
+        dataset = dataset.with_options(ignore_order)
+        dataset = dataset.map(self.read_tfrecord, num_parallel_calls=tf.data.AUTOTUNE)
+        return dataset
+
+    def get_dataset(self, train_set):
+        dataset = self.load_dataset(f"sRGB2XYZ/shards/{train_set}.tfrec")
+        dataset = dataset.shuffle(self.buffer_size)
+        dataset = dataset.batch(self.batch_size)
+        dataset = dataset.prefetch(tf.data.AUTOTUNE)
+        return dataset
--- src/model.py
+++ src/model.py
@@ -1,107 +1,13 @@
-import os
-
-import numpy as np
 import tensorflow as tf
 
 from tensorflow.keras import Model, Sequential
 from tensorflow.keras.layers import (
-    Layer, Conv2D, MaxPool2D, LeakyReLU, Flatten, Dense, Dropout, Resizing)
-
-class LocalSubNet(Layer):
-    def __init__(self, name, block_depth=16, conv_depth=32, scale=0.25):
-        super(LocalSubNet, self).__init__(name=name)
-        self.block_depth = block_depth
-        self.conv_depth = conv_depth
-        self.scale = scale
-        self.layer_config = {
-            "filters": self.conv_depth,
-            "kernel_size": 3,
-            "strides": 1,
-            "padding": "same",
-            "kernel_initializer": "he_normal",
-            "bias_initializer": "zeros"}
-
-    def build(self, input_shape):
-        self.net = Sequential()
-        # First layer
-        inputs = Conv2D(
-            **self.layer_config, input_shape=input_shape, 
-            activation=LeakyReLU(), name="conv_0")
-        self.net.add(inputs)
-
-        # Middle layers
-        for i in range(1, self.block_depth - 1):
-            conv = Conv2D(
-                **self.layer_config,
-                activation=LeakyReLU(),
-                name=f"conv_{i}")
-            self.net.add(conv)
-
-        # Last layer
-        outputs = Conv2D(
-            **self.layer_config, activation="tanh", 
-            name=f"conv_{self.block_depth}")
-        self.net.add(outputs)     
-
-    def call(self, x):
-        return self.net(x) * self.scale
-
-class GlobalSubNet(Layer):
-    def __init__(self, name, block_depth=16, conv_depth=32, image_size=128):
-        super(GlobalSubNet, self).__init__(name=name)
-        self.block_depth = block_depth
-        self.conv_depth = conv_depth
-        self.image_size = image_size
-        self.layer_config = {
-            "filters": self.conv_depth,
-            "kernel_size": 3,
-            "strides": 1,
-            "padding": "same",
-            "kernel_initializer": "he_normal",
-            "bias_initializer": "zeros"}
-
-    def build(self, input_shape):
-
-        self.net = Sequential()
-        # First layer
-        inputs = Conv2D(
-            **self.layer_config, input_shape=input_shape, 
-            activation=LeakyReLU(), name="conv_0")
-        self.net.add(inputs)
-
-        # Middle layers
-        for i in range(1, self.block_depth):
-            conv = Conv2D(
-                **self.layer_config,
-                activation=LeakyReLU(),
-                name=f"conv_{i}")
-            maxpool = MaxPool2D(pool_size=2, strides=2, padding="same", name=f"maxpool_{i}")
-            self.net.add(conv)
-            self.net.add(maxpool)
-
-        # Last layer
-        self.net.add(Flatten(name="flatten"))
-        self.net.add(Dense(1024, name="dense_0"))
-        self.net.add(Dense(1024, name="dense_1"))
-        self.net.add(Dropout(0.5, name="dropout_1"))
-        self.net.add(Dense(18, name="dense_3"))
-        
-    def call(self, x):
-        x = Resizing(
-            height=self.image_size, 
-            width=self.image_size,
-            interpolation='bilinear')(x)
-        return self.net(x)
+    Conv2D, MaxPool2D, LeakyReLU, Flatten, Dense, Dropout, Resizing, Rescaling)
 
 class CIEXYZNet(Model):
     def __init__(
-            self, 
-            local_depth=16, 
-            local_convdepth=32, 
-            global_depth=5, 
-            global_convdepth=64,
-            global_imagesize=128, 
-            scale=0.25):
+            self, local_depth, local_convdepth, global_depth, 
+            global_convdepth, global_imagesize, scale):
         super(CIEXYZNet, self).__init__()
         self.local_depth = local_depth
         self.local_convdepth = local_convdepth
@@ -110,33 +16,87 @@ class CIEXYZNet(Model):
         self.global_imagesize = global_imagesize
         self.scale = scale
 
+        self.conv_params = {
+            "kernel_size": 3,
+            "strides": 1,
+            "padding": "same",
+            "kernel_initializer": "he_normal",
+            "bias_initializer": "zeros"}        
+
+        self.srgb2xyz_localnet = self.get_localsubnet(name="sRGB2XYZ_localnet")
+        self.xyz2srgb_localnet = self.get_localsubnet(name="XYZ2sRGB_localnet")
+        self.srgb2xyz_globalnet = self.get_globalsubnet(name="sRGB2XYZ_globalnet")
+        self.xyz2srgb_globalnet = self.get_globalsubnet(name="XYZ2sRGB_globalnet")
+
+    def get_localsubnet(self, name):
+        local_subnet = Sequential(name=name)
+               
+        for i in range(self.local_depth):
+            local_subnet.add(Conv2D(
+                **self.conv_params,
+                filters=self.local_convdepth if i != self.local_depth - 1 else 3,
+                activation=LeakyReLU() if i != self.local_depth - 1 else "tanh",
+                name=f"conv_{i}"))
+
+        local_subnet.add(Rescaling(self.scale))
+        return local_subnet
+
+    def get_globalsubnet(self, name):
+        global_subnet = Sequential(name=name)
+
+        global_subnet.add(Resizing(
+            height=self.global_imagesize, 
+            width=self.global_imagesize,
+            interpolation='bilinear'))
+
+        for i in range(self.global_depth):
+            global_subnet.add(Conv2D(
+                **self.conv_params, 
+                filters=self.global_convdepth,
+                activation=LeakyReLU(), 
+                name=f"conv_{i}"))
+            global_subnet.add(MaxPool2D(
+                pool_size=2, strides=2, padding="valid", name=f"maxpool_{i}"))
+
+        global_subnet.add(Flatten(name="flatten_0"))
+        global_subnet.add(Dense(1024, name="dense_0"))
+        global_subnet.add(Dropout(0.5, name="dropout_0"))
+        global_subnet.add(Dense(18, name="dense_1"))
+        return global_subnet    
+
     def forward_local(self, x, target):
         if target == "xyz":
-            x = self.srgb2xyz_local_net(x)
+            x = self.srgb2xyz_localnet(x)
         elif target == "srgb":
-            x = self.xyz2srgb_local_net(x)
+            x = self.xyz2srgb_localnet(x)
         else:
             raise Exception("Wrong target")
         return x
 
+    def transform(self, x):
+        x = tf.reshape(x, (-1, 3))
+        x = tf.concat([x, tf.math.multiply(x, x)], axis=1)
+        return x
+
     def forward_global(self, x, target):
         if target == "xyz":
-            x_1 = self.srgb2xyz_global_net(x)
+            m_v = self.srgb2xyz_globalnet(x) # (8, 18)
         elif target == "srgb":
-            x_1 = self.xyz2srgb_global_net(x)
+            m_v = self.xyz2srgb_globalnet(x) # (8, 18)
         else:
             raise Exception("Wrong target")
 
-        x_1 = tf.reshape(x_1, (-1, 6, 3))
-        y = tf.identity(x)
-        for i in range(x_1.shape[0]):
-            x_1 = tf.squeeze(x_1[i, :, :, :])
-            x_1 = tf.reshape(x_1, (3, -1))
-            x_1 = tf.concat(x_1, x_1*x_1)
-            temp = tf.matmul(tf.squeeze(x_1[i, :, :]), x_1)
-            y[i, :, :, :] = tf.reshape(temp, x.shape[1:])           
-        return y
+        m = tf.reshape(m_v, (x.shape[0], 6, 3)) # (8, 6, 3)
 
+        y = []
+        for i in range(m.shape[0]):
+            t1 = self.transform(x[i]) # (65536, 6)
+            t2 = m[i] # (6, 3)
+            t = tf.matmul(t1, t2) # (65536, 3)
+            t =  tf.reshape(t, x.shape[1:]) # (256, 256, 3)
+            y.append(t)            
+        return tf.stack(y, axis=0)
+    
     def forward_srgb2xyz(self, srgb):
         l_xyz = srgb - self.forward_local(srgb, target='xyz')
         return self.forward_global(l_xyz, target='xyz')
@@ -145,21 +105,7 @@ class CIEXYZNet(Model):
         g_srgb = self.forward_global(xyz, target='srgb')
         return g_srgb + self.forward_local(g_srgb, target='srgb')
 
-    def forward(self, x):
+    def call(self, x):
         xyz = self.forward_srgb2xyz(x)
         srgb = self.forward_xyz2srgb(xyz)
         return xyz, srgb
-
-if __name__ == "__main__":
-    config = {
-        'local_depth': 16,
-        'local_convdepth': 32,
-        'global_depth': 5,
-        'global_convdepth': 64,
-        'global_imagesize': 128,
-        'scale': 0.25}
-
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-    model = CIEXYZNet(**config)
-    model
-
